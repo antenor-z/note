3f9278e6d10d1fe653746a2ebb19d0da7794c6fb
antenor-z <antenor-z@protonmail.com>
2025-02-14 16:38:09 -0300

    keyboard shortcuts and login fail

--- note-front/login.js
+++ note-front/login.js
@@ -1,6 +1,9 @@
 function login() {
     const username = document.getElementById("username").value
     const password = document.getElementById("password").value
+    document.getElementById("username").value = ""
+    document.getElementById("password").value = ""
+    document.getElementById('username').focus()
     fetch(`${window.API_URL}/login`,
         {
             method: "POST",
@@ -9,8 +12,17 @@ function login() {
         })
         .then(data => {
             if (data.status !== 200) {
+                document.getElementById('authfail').style.display = "block"
                 return
             }
             document.location = "/"
         })
 }
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        login()
+    }
+})
+window.addEventListener("load", () => {
+    document.getElementById('username').focus()
+})
