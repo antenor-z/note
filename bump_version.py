5756aa3818260034537b0471e5c307b42176c720
antenor-z <antenor-z@pm.me>
2025-03-01 11:56:20 -0300

    chore(bump_version): now detecting scope

--- bump_version.py
+++ bump_version.py
@@ -1,23 +1,28 @@
 import subprocess
+import re
 
-result = subprocess.run(["git", "log", "-1", "--pretty=%s"], capture_output=True, text=True)
+result = subprocess.run(["git", "log", "-1", "--pretty=%B"], capture_output=True, text=True)
 commit = result.stdout.strip()
 
 with open("VERSION", "r") as fp:
-    version = fp.read()
+    version = fp.read().strip()
     print(version)
 
 [major, minor, patch] = map(int, version.split("."))
 
-if "BREAKING CHANGE" in commit or commit.startswith("feat!:"):
+if "BREAKING CHANGE" in commit or re.search(r"(feat|fix)!:", commit):
     major += 1
     minor = 0
     patch = 0
-elif commit.startswith("feat:"):
+elif re.search(r"feat(\([a-zA-Z0-9]+\))?:", commit):
     minor += 1
     patch = 0
-elif commit.startswith("fix:"):
+elif re.search(r"fix(\([a-zA-Z0-9]+\))?:", commit):
     patch += 1
 
-with open("VERSION", "w") as fp:
-    fp.write(f"{major}.{minor}.{patch}")
+new_version = f"{major}.{minor}.{patch}"
+
+if new_version != version:
+    print(f"Bump version: {version} to {new_version}")
+    with open("VERSION", "w") as fp:
+        fp.write(f"{major}.{minor}.{patch}")
