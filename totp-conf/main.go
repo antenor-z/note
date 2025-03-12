216aae0bac37eda0f0bb7a71c7d5805bf1152c46
antenor-z <antenor-z@pm.me>
2025-03-11 21:02:55 -0300

    chore: totp-conf

--- totp-conf/main.go
+++ totp-conf/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"image/png"
 	"os"
 
 	"github.com/pquerna/otp/totp"
@@ -11,48 +9,40 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Printf("Usage: %s DOMAIN_NAME USER_NAME\n", os.Args[0])
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s USER_NAME\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	key, err := totp.Generate(totp.GenerateOpts{
-		Issuer:      os.Args[1],
-		AccountName: os.Args[2],
+		Issuer:      "note",
+		AccountName: os.Args[1],
 	})
 
 	if err != nil {
 		fmt.Print("error while creating totp key")
-		return
+		os.Exit(1)
 	}
 
-	var buf bytes.Buffer
-	img, err := key.Image(50, 50)
-	if err != nil {
-		fmt.Print("error while creating totp qr code image")
-		return
-	}
-	png.Encode(&buf, img)
-
-	display(buf.Bytes())
+	display(key.URL())
 
 	var passcode string
 	fmt.Print("Type the generated code: ")
-	fmt.Scanf("%s", passcode)
+	fmt.Scanf("%s", &passcode)
 
 	if !totp.Validate(passcode, key.Secret()) {
 		fmt.Println("Failed to verify TOTP")
 		os.Exit(1)
 	}
 	fmt.Println("Add to config.toml:")
-	fmt.Printf("totpKey=%s", key.Secret())
+	fmt.Printf("totp=%s\n", key.Secret())
 }
 
-func display(data []byte) {
-	qr, err := qrcode.New(string(data), qrcode.Low)
+func display(data string) {
+	qr, err := qrcode.New(data, qrcode.Low)
 	if err != nil {
 		fmt.Println("Failed to generate ASCII QR code:", err)
-		return
+		os.Exit(1)
 	}
 
 	fmt.Println(qr.ToString(true))
