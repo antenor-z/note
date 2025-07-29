da3eae0b02ff07d84edf35e5abad195131fbdc6e
antenor-z <antenor-z@pm.me>
2025-07-29 14:50:24 -0300

    fix: search

--- front/js/search.js
+++ front/js/search.js
@@ -1,13 +1,13 @@
 document.getElementById("search").addEventListener("input", function () { search(this) })
 
 function search(inputElement) {
-    const searchText = inputElement.value.toLowerCase();
+    const searchText = inputElement.value.toLowerCase().trim();
     const notes = document.getElementById("notes-container").children
     for (let i = 0; i < notes.length; i += 1) {
         const note = notes[i]
         const noteInner = note.children[1]
         const noteTitle = noteInner.children[0].innerText.toLowerCase()
-        const noteContent = noteInner.children[3].innerHTML.toLowerCase()
+        const noteContent = noteInner.children[1].innerText.toLowerCase()
         if (noteTitle.includes(searchText) || noteContent.includes(searchText)) {
             note.style.display = "block"
         }
